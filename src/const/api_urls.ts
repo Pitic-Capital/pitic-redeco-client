144b7ed7ea7b3f57c57ba2d6a2846a26d6806c8b
Cesar <admingit@lecosy.com.mx>
2026-02-24 18:45:14 -0700

    Updates

--- src/const/api_urls.ts
+++
@@ -1,9 +0,0 @@
-export const API_URL_TEST = "https://api.condusef.gob.mx";
-export const API_URL_PROD = "https://api-redeco.condusef.gob.mx";
-
-export const ENV_KEY = "APP_ENV";
-
-export const getApiUrl = (): string => {
-   const env = localStorage.getItem(ENV_KEY);
-   return env === "test" ? API_URL_TEST : API_URL_PROD;
-};
